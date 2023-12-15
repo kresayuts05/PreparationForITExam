56166f268f386fe198b6da15b0ef15750f7d06f3
kresa tsvetkova <kresayuts@gmail.com>
2023-12-15 02:52:52 +0200

    design

--- PreparationForITExam/wwwroot/js/custom.js
+++ PreparationForITExam/wwwroot/js/custom.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
+$(document).ready(function () {
 	"use strict";
-    
-        /*==================================
+
+	/*==================================
 * Author        : "ThemeSine"
 * Template Name : Listrace directory HTML Template
 * Version       : 1.0
@@ -10,102 +10,102 @@ $(document).ready(function(){
 
 
 
-/*=========== TABLE OF CONTENTS ===========
-1. Scroll To Top 
-2. slick carousel
-3. welcome animation support
-4. feather icon
-5. counter
-======================================*/
+	/*=========== TABLE OF CONTENTS ===========
+	1. Scroll To Top 
+	2. slick carousel
+	3. welcome animation support
+	4. feather icon
+	5. counter
+	======================================*/
 
-    // 1. Scroll To Top 
-		$(window).on('scroll',function () {
-			if ($(this).scrollTop() > 600) {
-				$('.return-to-top').fadeIn();
-			} else {
-				$('.return-to-top').fadeOut();
-			}
-		});
-		$('.return-to-top').on('click',function(){
-				$('html, body').animate({
-				scrollTop: 0
-			}, 1500);
-			return false;
-		});
-	
-	
+	// 1. Scroll To Top 
+	$(window).on('scroll', function () {
+		if ($(this).scrollTop() > 600) {
+			$('.return-to-top').fadeIn();
+		} else {
+			$('.return-to-top').fadeOut();
+		}
+	});
+	$('.return-to-top').on('click', function () {
+		$('html, body').animate({
+			scrollTop: 0
+		}, 1500);
+		return false;
+	});
+
+
 	// 2. slick carousel
 
-	    $(".testimonial-carousel").slick({
-	        infinite: true,
-	        centerMode: true,
-	        autoplay:true,
-	        slidesToShow: 5,
-	        slidesToScroll: 3,
-	        autoplaySpeed:1500,
-	        // the magic
-			responsive: [
-				{
+	$(".testimonial-carousel").slick({
+		infinite: true,
+		centerMode: true,
+		autoplay: true,
+		slidesToShow: 5,
+		slidesToScroll: 3,
+		autoplaySpeed: 1500,
+		// the magic
+		responsive: [
+			{
 
-					breakpoint:1440,
-					settings: {
-					slidesToShow:3
-					}
+				breakpoint: 1440,
+				settings: {
+					slidesToShow: 3
+				}
 
-				},
-				{
+			},
+			{
 
-					breakpoint: 1024,
-					settings: {
-					slidesToShow:2,
-					
-					}
-
-				}, 
-				{
-
-					breakpoint:991,
-					settings: {
-					slidesToShow:2,
-					centerMode:false,
-					}
-
-				},
-				{
-
-					breakpoint:767,
-					settings: {
-					slidesToShow:1,
-					}
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 2,
 
 				}
-			]
-	    });
+
+			},
+			{
+
+				breakpoint: 991,
+				settings: {
+					slidesToShow: 2,
+					centerMode: false,
+				}
+
+			},
+			{
+
+				breakpoint: 767,
+				settings: {
+					slidesToShow: 1,
+				}
+
+			}
+		]
+	});
 
 
 
-    // 3. welcome animation support
+	// 3. welcome animation support
 
-        $(window).load(function(){
-        	$(".welcome-hero-txt h2,.welcome-hero-txt p").removeClass("animated fadeInUp").css({'opacity':'0'});
-            $(".welcome-hero-serch-box").removeClass("animated fadeInDown").css({'opacity':'0'});
-        });
+	$(window).load(function () {
+		$(".welcome-hero-txt h2,.welcome-hero-txt p").removeClass("animated fadeInUp").css({ 'opacity': '0' });
+		$(".welcome-hero-serch-box").removeClass("animated fadeInDown").css({ 'opacity': '0' });
+	});
 
-        $(window).load(function(){
-        	$(".welcome-hero-txt h2,.welcome-hero-txt p").addClass("animated fadeInUp").css({'opacity':'0'});
-            $(".welcome-hero-serch-box").addClass("animated fadeInDown").css({'opacity':'0'});
-        });
+	$(window).load(function () {
+		$(".welcome-hero-txt h2,.welcome-hero-txt p").addClass("animated fadeInUp").css({ 'opacity': '0' });
+		$(".welcome-hero-serch-box").addClass("animated fadeInDown").css({ 'opacity': '0' });
+	});
 
 	// 4. feather icon
 
-		feather.replace();
+	feather.replace();
 
 	// 5. counter
-		$(window).on('load', function(){	
-			$('.counter').counterUp({
-				delay: 10,
-				time: 3000
-			});	
+	$(window).on('load', function () {
+		$('.counter').counterUp({
+			delay: 10,
+			time: 3000
 		});
+	});
 
 });
--- PreparationForITExam/wwwroot/js/main.js
+++ PreparationForITExam/wwwroot/js/main.js
@@ -292,37 +292,37 @@
     }; // endSmoothScroll
 
 
-    /* AjaxChimp
-     * ------------------------------------------------------ */
-    const ssAjaxChimp = function () {
+    ///* AjaxChimp
+    // * ------------------------------------------------------ */
+    //const ssAjaxChimp = function () {
 
-        $('#mc-form').ajaxChimp({
-            language: 'es',
-            url: cfg.mailChimpURL
-        });
+    //    $('#mc-form').ajaxChimp({
+    //        language: 'es',
+    //        url: cfg.mailChimpURL
+    //    });
 
-        // Mailchimp translation
-        //
-        //  Defaults:
-        //	 'submit': 'Submitting...',
-        //  0: 'We have sent you a confirmation email',
-        //  1: 'Please enter a value',
-        //  2: 'An email address must contain a single @',
-        //  3: 'The domain portion of the email address is invalid (the portion after the @: )',
-        //  4: 'The username portion of the email address is invalid (the portion before the @: )',
-        //  5: 'This email address looks fake or invalid. Please enter a real email address'
+    //    // Mailchimp translation
+    //    //
+    //    //  Defaults:
+    //    //	 'submit': 'Submitting...',
+    //    //  0: 'We have sent you a confirmation email',
+    //    //  1: 'Please enter a value',
+    //    //  2: 'An email address must contain a single @',
+    //    //  3: 'The domain portion of the email address is invalid (the portion after the @: )',
+    //    //  4: 'The username portion of the email address is invalid (the portion before the @: )',
+    //    //  5: 'This email address looks fake or invalid. Please enter a real email address'
 
-        $.ajaxChimp.translations.es = {
-            'submit': 'Submitting...',
-            0: '<i class="fa fa-check"></i> We have sent you a confirmation email',
-            1: '<i class="fa fa-warning"></i> You must enter a valid e-mail address.',
-            2: '<i class="fa fa-warning"></i> E-mail address is not valid.',
-            3: '<i class="fa fa-warning"></i> E-mail address is not valid.',
-            4: '<i class="fa fa-warning"></i> E-mail address is not valid.',
-            5: '<i class="fa fa-warning"></i> E-mail address is not valid.'
-        }
+    //    $.ajaxChimp.translations.es = {
+    //        'submit': 'Submitting...',
+    //        0: '<i class="fa fa-check"></i> We have sent you a confirmation email',
+    //        1: '<i class="fa fa-warning"></i> You must enter a valid e-mail address.',
+    //        2: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+    //        3: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+    //        4: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+    //        5: '<i class="fa fa-warning"></i> E-mail address is not valid.'
+    //    }
 
-    }; // end ssAjaxChimp
+    //}; // end ssAjaxChimp
 
 
     /* back to top
@@ -409,7 +409,7 @@
         ssBricksAnimate();
         ssSlickSlider();
         ssSmoothScroll();
-        ssAjaxChimp();
+     /*   ssAjaxChimp();*/
         ssBackToTop();
         gameDeveloperCheckBoxClick();
     })();
